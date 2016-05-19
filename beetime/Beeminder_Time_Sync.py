f83ccf0c376b5d5fcea4eee73fca45df697a80c7
Ian McB <yanmcbe@gmail.com>
2016-05-19 20:25:43 +0200

    beetime: remove duplicated headers

--- beetime/Beeminder_Time_Sync.py
+++ beetime/Beeminder_Time_Sync.py
@@ -1,8 +1,3 @@
-# This add-on sends your time spent reviewing to Beeminder.
-# Copyright: Yan McB <yanmcbe@gmail.com>
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Version: v1.2
-
 BEE = 'bee_conf' # name of key in anki configuration dict
 
 from Beeminder_Settings import BeeminderSettings
--- beetime/__init__.py
+++ beetime/__init__.py
@@ -1,6 +1,1 @@
-# This add-on sends your time spent reviewing to Beeminder.
-# Copyright: Yan McB <yanmcbe@gmail.com>
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Version: v1.2
-
 import Beeminder_Time_Sync
