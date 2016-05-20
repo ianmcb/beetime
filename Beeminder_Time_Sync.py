0b37ae6e8995ea423214b7afc7b32afc10f5304a
Ian McB <yanmcbe@gmail.com>
2016-05-20 16:50:27 +0200

    beetime: change add-on description

--- Beeminder_Time_Sync.py
+++ Beeminder_Time_Sync.py
@@ -1,4 +1,4 @@
-# This add-on sends your time spent reviewing to Beeminder.
+# Description: Allows one to send time spent reviewing to Beeminder.
 # Copyright: Ian McB <yanmcbe@gmail.com>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 # Version: v1.2
