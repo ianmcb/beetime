92aec4e9cb124cd0d8b85c2debe4faaa3426eb31
Ian McB <yanmcbe@gmail.com>
2016-05-21 01:46:43 +0200

    beetime: bump version to 1.4

--- Beeminder_Time_Sync.py
+++ Beeminder_Time_Sync.py
@@ -1,6 +1,6 @@
 # Description: Allows one to send time spent reviewing to Beeminder.
 # Copyright: Ian McB <yanmcbe@gmail.com>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Version: v1.2.1
+# Version: v1.4
 
 import beetime
