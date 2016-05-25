cb4779835671319d1bd3210dabcbd4e4833541af
Ian McB <yanmcbe@gmail.com>
2016-05-25 23:08:34 +0200

    beetime: bump version to v1.7 for due cards

--- Beeminder_Sync.py
+++ Beeminder_Sync.py
@@ -2,6 +2,6 @@
 #              added to Beeminder.
 # Copyright: Ian McB <yanmcbe@gmail.com>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Version: v1.6.2
+# Version: v1.7
 
 import beetime
