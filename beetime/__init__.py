826d7057b43a3d03b4b32a6d1d9db912f8c41235
Ian McB <yanmcbe@gmail.com>
2016-06-02 19:16:20 +0200

    beetime: remove implementation documentation

--- beetime/__init__.py
+++ beetime/__init__.py
@@ -58,65 +58,6 @@ mw.toolbar._rightIconsList = wrap(mw.toolbar._rightIconsList, toolbarRightIconsL
 
 # global sync shortcut
 # --------------------
-"""
-First approach trying to implement this.
-
-mw.keyPressEvent(self, evt) looks for mw.keyHandler, which is
-initialized as None. my thinking is we add/override mw's keyHandler()
-method to look for the desired key, convert it using `key =
-unicode(evt.text())` and run our desired function. Return True if we
-"ate" the key, False if we didn't, so keyPressEvent() can continue to go
-through its logic. This is my first attempt at describing what I'll do
-before I do it; will need to check if it's a useful practice.
-
-Source: aqt/main.py
-
-Maybe, I need to wrap mw.setupKeys, since that one inits keyHandler to
-None every time it is called.
-
-This doesn't work, because keyHandler is designed to be overriden by
-other objects, like the DeckBrowser, presumably to make context-specific
-shortcuts possible. We don't really need sync with Beeminder to be a
-global shortcut, so we could try overriding a more specific keyHandler,
-like DeckBrowser's, which doesn't do anything.
-"""
-#def addSyncShortcut(event):
-#    shortcut = "M" # Shift+M
-#    key = unicode(event.text())
-#    if key == shortcut:
-#        syncDispatch(at='manual')
-#        return True
-#    return False
-#mw.keyHandler = addSyncShortcut
-#def mySetupKeys(self):
-#    self.keyHandler = addSyncShortcut
-#def wrapSetupKeys(self):
-#    self.setupKeys = wrap(self.setupKeys, lambda s=mw: mySetupKeys(s), 'after')
-#    self.setupUI()
-#addHook("profileLoaded", lambda s=mw: wrapSetupKeys(s))
-
-"""
-New approach, trying to manually wrap (without using any convenience
-functions) the most high level (I think) key handler, keyPressEvent.
-
-This works!
-"""
-#def syncHandler(event):
-#    shortcut = "M" # Shift+M
-#    key = unicode(event.text())
-#    if key == shortcut:
-#        event.accept()
-#        syncDispatch(at='manual')
-#        return
-#    return oldEventHandler(event)
-#
-#oldEventHandler = mw.keyPressEvent
-#mw.keyPressEvent = myEventHandler
-
-"""
-Third approach, making my shortcut specific to the deck browser.
-We don't need to bother with the accept flag of the QEvent object.
-"""
 def syncKeyHandler(self, event):
     syncShortcut = "M" # Shift+M
     keyPress = unicode(event.text())
